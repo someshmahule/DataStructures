7cdb543e1527301eabb4bf38cc2f6e38f34b594d
Somesh Mahule <someshmahule@gmail.com>
2020-02-29 20:18:19 +0530

    DS-LL-1 Adding more Operations Like : 	1. Delete a Link List 	2. Find length of Link List 	3. Search an Element in Link List 	4. Function to get Nth Node

--- LinkList.py
+++ LinkList.py
@@ -1,3 +1,9 @@
+#
+#   Code To Learn Link List Operations
+#   
+#   -Somesh Mahule
+#
+
 class Node():
 
     def __init__(self, data) :
@@ -9,12 +15,15 @@ class LinkList():
     def __init__(self, head) :
         self.head = head
 
+    
+    #function to Print List
     def printList(self):
         start = self.head
         while(start):
             print(start.data)
             start = start.next
 
+    #Function to Add Node into a Link List
     def add(self, data):
         if self.head == None:
             top = Node(data)
@@ -26,6 +35,7 @@ class LinkList():
         tail = Node(data)
         start.next = tail
 
+    #Function to Add Node at start of Link List
     def addAtStart(self, data):
         top = Node(data)
         if self.head == None:
@@ -34,8 +44,86 @@ class LinkList():
         tmp = self.head
         self.head = top
         top.next = tmp
-            
 
+    #Function to Check if Link List exist
+    def isList(self):
+        if self.head == None:
+            return False
+        else:
+            return True
+
+    #Fucntion to Delete Head Of a Link List
+    def delHead(self):
+        if self.isList():
+            tmp = self.head
+            self.head = tmp.next
+            return
+        else:
+            print("Empty List")
+
+    #Function to Delete Node Particular Position
+    def delI(self, ind):
+        i=0
+        if self.isList():
+            if ind == 0:
+                self.delHead()
+                return
+            start = self.head
+            while(i!=ind):
+                i = i+1
+            tmp = start.next
+            start.next = tmp.next
+            start = start.next
+    
+    #Function to Delete Node of a Particular Key
+    def delKey(self, key):
+        if self.isList():
+            if key == self.head.data :
+                return self.delHead()
+            else:
+                itr = self.head
+                while (itr.next.data != key) :
+                    itr = itr.next
+                tmp = itr.next
+                itr.next = tmp.next
+
+    #Function to Delete Whole Link List
+    def delList(self):
+        self.head = None
+        return
+
+    #Function to Get Length of Link List
+    def getLength(self):
+        if self.isList() == False:
+            return 0
+        else:
+            l = 0
+            itr = self.head
+            while(itr):
+                itr = itr.next
+                l = l+1
+            return l
+
+    def searchKey(self,key):
+        if self.isList():
+            itr = self.head
+            while(itr.next!=None and itr.data != key):
+                itr = itr.next
+            if itr.next == None:
+                return False
+            else:
+                return True
+
+    def getNthNode(self, n):
+        if self.isList():
+            itr = self.head
+            i=0
+            while(i!=n):
+                itr = itr.next
+                i = i+1
+            return itr.data
+
+#Driver Fucntion for Link List Operations
 def main():
     a = Node(1)
     ll = LinkList(a)
@@ -47,6 +135,23 @@ def main():
     ll.addAtStart(0)
     print("LinkList\n")
     ll.printList()
+    ll.delI(1)
+    print("After Delete")
+    ll.printList()
+    ll.delKey(0)
+    print("After Delete")
+    ll.printList()
+    print("Length of List ",ll.getLength())
+    ll.printList()
+    print("Enter key to Search")
+    key = int(input())
+    print(ll.searchKey(key))
+    print("Give Nth Node to Search")
+    n = int(input())
+    print("Nth Node is ",ll.getNthNode(n))
+    ll.delList()
+    print("After Link List Delete")
+
 
 if __name__ == "__main__":
     main()
